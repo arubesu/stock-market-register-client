edd7b3917ab41e4364e6a0a670c29100a4a80c1d
Bruno Souza <bruno.arubesu@gmail.com>
2018-12-27 15:49:45 -0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientManagerComponent } from './components/client-manager/client-manager.component';
+import { StockManagerComponent } from './components/stock-manager/stock-manager.component';
+import { OrderManagerComponent } from './components/order-manager/order-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientManagerComponent,
+    StockManagerComponent,
+    OrderManagerComponent
   ],
   imports: [
     BrowserModule
